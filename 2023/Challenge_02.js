55411fb86b07014692a506d2896302bcf446194e
GARCEL_AMSYSTEM\Joaqu <jgarcel@amsystem.es>
2023-11-13 21:03:43 +0100

    [FIX] formato al codigo

--- 2023/Challenge_02.js
+++ 2023/Challenge_02.js
@@ -1,18 +1,18 @@
 function compilator() {
-    const message =
-        "&###@&*&###@@##@##&######@@#####@#@#@#@##@@@@@@@@@@@@@@@*&&@@@@@@@@@####@@@@@@@@@#########&#&##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@&"
+  const message =
+    "&###@&*&###@@##@##&######@@#####@#@#@#@##@@@@@@@@@@@@@@@*&&@@@@@@@@@####@@@@@@@@@#########&#&##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@&";
 
-    let cont = 0
-    let response = ""
-    const chartsMessaje = message.split("")
-    chartsMessaje.forEach(chart=>{
-        switch (chart){
-            case "#": cont++; break;
-            case "@": cont--; break;
-            case "*": cont *= cont; break;
-            case "&": response += cont; break;
-        }
-    })
-    return response 
+  let cont = 0;
+  let response = "";
+  const chartsMessaje = message.split("");
+  chartsMessaje.forEach((chart) => {
+    switch (chart) {
+      case "#": cont++; break;
+      case "@": cont--; break;
+      case "*": cont *= cont; break;
+      case "&": response += cont; break;
+    }
+  });
+  return response;
 }
-console.log(compilator())
+console.log(compilator());
